206667069d6eff8d59693c3ec2d19cc7251e496f
Rafaa Seddik <rafaaseddik@yahoo.fr>
2017-07-10 20:22:22 +0100

    [RAFAA][Main Sections Front end]

---
+++ iops/front-end/components/news.php
@@ -0,0 +1,104 @@
+<div class="section back-white" id="news">
+
+
+
+    <div class="section z-big">
+        <div class="container">
+            <div class="twelve columns remove-bottom">
+                <div class="title-text left">
+                    <h3>Latest News</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<div class="news-carousel-wrap" >
+    <div id="sync1" class="owl-carousel">
+        <div class="item background-news-image-1">
+            <div class="news-mask"></div>
+            <div class="news-text">
+                <div class="container ">
+                    <div class="twelve columns">
+                        <h1>We build<br>great brands.</h1><p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula quam, aliquam sit amet felis ut,
+                            scelerisque ultrices odio. Nullam commodo lorem sit amet mi sollicitudin, eget pharetra urna iaculis.
+                        </p>
+                    </div>
+                    MMAN
+                </div>
+            </div>
+        </div>
+        <div class="item background-news-image-2">
+            <div class="news-mask"></div>
+            <div class="news-text">
+                <div class="container ">
+                    <div class="twelve columns">
+                        <h1>Focused<br>on strategy.</h1><p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula quam, aliquam sit amet felis ut,
+                            scelerisque ultrices odio. Nullam commodo lorem sit amet mi sollicitudin, eget pharetra urna iaculis.
+                        </p>
+                    </div>
+                    MMAN
+                </div>
+            </div>
+        </div>
+        <div class="item background-news-image-3">
+            <div class="news-mask"></div>
+            <div class="news-text">
+                <div class="container ">
+                    <div class="twelve columns">
+                        <h1>Specialized in<br>brand experience.</h1><p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula quam, aliquam sit amet felis ut,
+                            scelerisque ultrices odio. Nullam commodo lorem sit amet mi sollicitudin, eget pharetra urna iaculis.
+                        </p>
+                    </div>
+                    MMAN
+                </div>
+            </div>
+        </div>
+        <div class="item background-news-image-3">
+            <div class="news-mask"></div>
+            <div class="news-text">
+                <div class="container ">
+                    <div class="twelve columns">
+                        <h1>Specialized in<br>brand experience.</h1><p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula quam, aliquam sit amet felis ut,
+                            scelerisque ultrices odio. Nullam commodo lorem sit amet mi sollicitudin, eget pharetra urna iaculis.
+                        </p>
+                    </div>
+                    MMAN
+                </div>
+            </div>
+        </div>
+        <div class="item background-news-image-3">
+            <div class="news-mask"></div>
+            <div class="news-text">
+                <div class="container ">
+                    <div class="twelve columns">
+                        <h1>Specialized in<br>brand experience.</h1><p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ligula quam, aliquam sit amet felis ut,
+                            scelerisque ultrices odio. Nullam commodo lorem sit amet mi sollicitudin, eget pharetra urna iaculis.
+                        </p>
+                    </div>
+                    MMAN
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="sync2" class="owl-carousel ">
+        <div class="item">
+        </div>
+        <div class="item">
+        </div>
+        <div class="item">
+        </div>
+        <div class="item">
+        </div>
+        <div class="item">
+        </div>
+    </div>
+</div>
+</div>
